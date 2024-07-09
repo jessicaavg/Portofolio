4743c11cccfc9d4253e7e70387b9348986457ec6
jessicaavg <veganainggolan9560@gmail.com>
2024-07-09 17:51:29 +0700

    Profile

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,19 +7,18 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Jessica</h1>
+        <h1 className={styles.title}>Hi, I'm Jessica Vega</h1>
         <p className={styles.description}>
-        I am a 6th semester student majoring in informatics from Universitas Amikom Yogyakarta 
-        with an interest in data science and machine learning. Experienced in learning and 
-        applying various deep learning and machine learning algorithms. Have skills in the Python 
-        programming language and have the dedication to become a machine learning engineer in the future.
+        I am a 6th semester student with a passion for technology and design. My interests are focused on web development, especially <b>frontend, data processing, and UI/UX design. </b> 
+        I am experienced in learning and applying various deep learning and machine learning algorithms in several projects and problems, and proficient in <b>Python. </b> In my personal 
+        exploration, I have developed the ability to create intuitive and engaging user interfaces. I enjoy being creative in creating attractive and user-friendly <b>UI/UX designs</b>.
         </p>
-        <a href="mailto:veganainggolan9560@gmail.com" className={styles.contactBtn}>
-          Contact Me
+        <a href="https://drive.google.com/file/d/1TKj8-z75n1qJjnCQMuaePdF59wgnirkp/view?usp=drive_link" className={styles.contactBtn}>
+          My Resume
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/image.png")}
         alt="Hero image of me"
         className={styles.heroImg}
       />
@@ -28,3 +27,4 @@ export const Hero = () => {
     </section>
   );
 };
+
